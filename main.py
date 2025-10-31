2c188f5a602336c7ce488eb49799dedcd1779a22
VVA122334 <agrawalvishwesh209@gmail.com>
2025-10-31 08:33:52 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+
+class ArticleInput(BaseModel):
+    url: str
 
 @app.get("/")
 def home():
     return {"message": "Your FastAPI server is running!"}
+
+@app.post("/process")
+def process_article(data: ArticleInput):
+    # temporary testing response
+    return {
+        "received_url": data.url,
+        "status": "OK"
+    }
